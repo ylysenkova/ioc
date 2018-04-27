1436be72d6a1703353128875b7c57e66f077e354
Yana <yana.lysenkova@gmail.com>
2018-04-27 15:42:26 +0300

    Bean, BeanDefinition entities are created

---
+++ src/main/java/com.lysenkova.initbean/entity/Bean.java
@@ -0,0 +1,38 @@
+package com.lysenkova.initbean.entity;
+
+public class Bean {
+    private Object value;
+    private String id;
+
+    public Bean() {
+    }
+
+    public Bean(Object value, String id) {
+        this.value = value;
+        this.id = id;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Bean{" +
+                "value=" + value +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com.lysenkova.initbean/entity/BeanDefinition.java
@@ -0,0 +1,62 @@
+package com.lysenkova.initbean.entity;
+
+import java.util.Map;
+
+public class BeanDefinition {
+    private String id;
+    private String beanClassName;
+    private Map<String, String> dependencies;
+    private Map<String, String> refDependencies;
+
+    public BeanDefinition() {
+    }
+
+    public BeanDefinition(String id, String beanClassName, Map<String, String> dependencies, Map<String, String> refDependencies) {
+        this.id = id;
+        this.beanClassName = beanClassName;
+        this.dependencies = dependencies;
+        this.refDependencies = refDependencies;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBeanClassName() {
+        return beanClassName;
+    }
+
+    public void setBeanClassName(String beanClassName) {
+        this.beanClassName = beanClassName;
+    }
+
+    public Map<String, String> getDependencies() {
+        return dependencies;
+    }
+
+    public void setDependencies(Map<String, String> dependencies) {
+        this.dependencies = dependencies;
+    }
+
+    public Map<String, String> getRefDependencies() {
+        return refDependencies;
+    }
+
+    public void setRefDependencies(Map<String, String> refDependencies) {
+        this.refDependencies = refDependencies;
+    }
+
+    @Override
+    public String toString() {
+        return "BeanDefinition{" +
+                "id='" + id + '\'' +
+                ", beanClassName='" + beanClassName + '\'' +
+                ", dependencies=" + dependencies +
+                ", refDependencies=" + refDependencies +
+                '}';
+    }
+}
