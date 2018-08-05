082f660137e021c1821c4afa798dcc5cd87500cd
Yana <yana.lysenkova@gmail.com>
2018-08-05 18:54:28 +0300

    Fix XML parser

--- src/main/java/com/lysenkova/ioc/beanparser/BeanDefinitionReader.java
+++ src/main/java/com/lysenkova/ioc/beanparser/BeanDefinitionReader.java
@@ -2,6 +2,7 @@ package com.lysenkova.ioc.beanparser;
 
 import com.lysenkova.ioc.entity.BeanDefinition;
 
+import java.io.InputStream;
 import java.util.List;
 
 public interface BeanDefinitionReader {
--- src/main/java/com/lysenkova/ioc/beanparser/xml/XMLBeanDefinitionReader.java
+++ src/main/java/com/lysenkova/ioc/beanparser/xml/XMLBeanDefinitionReader.java
@@ -7,6 +7,8 @@ import com.lysenkova.ioc.exception.BeanInstantiationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,19 +16,19 @@ import java.util.List;
 public class XMLBeanDefinitionReader implements BeanDefinitionReader {
     private static final SAXParserFactory SAX_PARSER_FACTORY = SAXParserFactory.newInstance();
 
-    private String[] paths;
+    private InputStream[] files;
 
     public XMLBeanDefinitionReader(String[] paths) {
         ClassLoader classLoader = getClass().getClassLoader();
+        InputStream[] filesContent = new InputStream[paths.length];
         for (int i = 0; i < paths.length; i++) {
-            URL resource = classLoader.getResource(paths[i]);
+            InputStream resource = classLoader.getResourceAsStream(paths[i]);
             if (resource == null) {
                 throw new RuntimeException("Can not find file.");
             }
-            File file = new File(resource.getFile());
-            paths[i] = file.getAbsolutePath();
+            filesContent[i] = resource;
         }
-        this.paths = paths;
+        this.files = filesContent;
     }
 
     public List<BeanDefinition> readBeanDefinitions() throws BeanInstantiationException {
@@ -34,14 +36,15 @@ public class XMLBeanDefinitionReader implements BeanDefinitionReader {
             SAXParser saxParser = SAX_PARSER_FACTORY.newSAXParser();
             List<BeanDefinition> beanDefinitions = new ArrayList<>();
 
-            for (String path : paths) {
+            for (InputStream file : files) {
                 BeanParserHandler beanParserHandler = new BeanParserHandler();
-                saxParser.parse(path, beanParserHandler);
+                System.out.println(file);
+                saxParser.parse(file, beanParserHandler);
                 beanDefinitions.addAll(beanParserHandler.getBeanDefinitions());
             }
             return beanDefinitions;
         } catch (Exception e) {
-            throw new RuntimeException("Exception during parsing file with beans.");
+            throw new RuntimeException("Exception during parsing file with beans.", e);
         }
     }
 }
